fcf319fe2c7ee530e327375ce3d96192c8cfd89c
juanpabl0svn <juanpabl0svn@gmail.com>
2024-11-02 18:06:47 +0000

    mejora de estilo

--- lib/application/screens/user-screen.dart
+++ lib/application/screens/user-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myapp/domain/models/state-result.dart';
 import 'package:myapp/infraestructure/providers/user.provider.dart';
 
 class UserScreen extends ConsumerStatefulWidget {
@@ -13,7 +14,6 @@ class _UserScreenState extends ConsumerState<UserScreen> {
   @override
   void initState() {
     super.initState();
-    // Asegurarse de que el provider tenga acceso al context
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ref.read(userProvider.notifier).setContext(context);
     });
@@ -22,7 +22,6 @@ class _UserScreenState extends ConsumerState<UserScreen> {
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(userProvider);
-
     final bool hasNoData = state.list.isEmpty;
 
     return Scaffold(
@@ -34,38 +33,51 @@ class _UserScreenState extends ConsumerState<UserScreen> {
             onPressed: state.isLoading
                 ? null
                 : () => ref.read(userProvider.notifier).cleanAndGetFromCache(),
-          )
+          ),
         ],
       ),
-      body: state.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : hasNoData
-              ? const Center(child: Text('No hay datos guardados'))
-              : ListView.builder(
-                  itemCount: state.list.length,
-                  itemBuilder: (context, index) {
-                    final user = state.list[index];
-                    return ListTile(
-                      title: Text(user.name),
-                      subtitle: Text(user.email),
-                    );
-                  },
-                ),
+      body: _buildBody(state, hasNoData),
       floatingActionButton: FloatingActionButton(
         onPressed: state.isLoading
             ? null
-            : () => {
-                  if (hasNoData)
-                    {
-                      ref.read(userProvider.notifier).fetchUsersFromApi(),
-                    }
-                  else
-                    {
-                      ref.read(userProvider.notifier).deleteData(),
-                    }
-                },
+            : () => hasNoData
+                ? ref.read(userProvider.notifier).fetchUsersFromApi()
+                : ref.read(userProvider.notifier).deleteData(),
         child: Icon(hasNoData ? Icons.search : Icons.delete),
       ),
     );
+  }
+
+  Widget _buildBody(StateResult state, bool hasNoData) {
+    if (state.isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    } else if (hasNoData) {
+      return const Center(child: Text('No hay datos guardados'));
+    } else {
+      return ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        itemCount: state.list.length,
+        itemBuilder: (context, index) {
+          final user = state.list[index];
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            elevation: 3,
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text(user.name[0].toUpperCase()),
+              ),
+              title: Text(
+                user.name,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              subtitle: Text(user.email),
+            ),
+          );
+        },
+      );
+    }
   }
 }
